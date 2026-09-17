82f329c4ac69810c76b81c706bd63230f3e69b11
mo-lab <55297854+mo-lab@users.noreply.github.com>
2026-09-17 10:24:08 +0330

    static

---
+++ kmes/staticfiles/js/service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'equipment-tracker-v1';
+const urlsToCache = [
+    '/',
+    '/static/css/bootstrap.min.css',
+    '/static/js/bootstrap.bundle.min.js',
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
